222389bc718a781a09ab4d1f1a5c936fc088eb5b
kannan5 <kannanhlr@gmail.com>
2018-11-01 00:38:25 +0530

    Morphological operation

--- morph_ops.py
+++ morph_ops.py
@@ -1,17 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-def morphological_operations():
-    img = cv.imread(r'E:\pyworks\images.png')
-    kernel = np.ones((5, 5), np.uint8)
-    img_erosion = cv.erode(img, kernel=kernel, iterations=1)
-    img_dilate = cv.dilate(img, kernel=kernel, iterations=1)
-    cv.imshow('img_erosion', img_erosion)
-    cv.imshow('img_dilate', img_dilate)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-
 def opening_n_closing():
     img = cv.imread(r'E:\pyworks\coin-detection.jpg')
     img = cv.resize(img, (700, 700))
@@ -38,5 +27,3 @@ def detect():
 
 #detect()
 opening_n_closing()
-
-#morphological_operations()
